6b31e6613604de207e1331707ab3ce0603153761
Jason <jasonventresca@gmail.com>
2018-02-17 20:53:52 -0500

    Playing around with props.children

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -3,5 +3,28 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  );
+}
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+);
+
+
+ReactDOM.render((
+    <Layout>
+      {template}
+    </Layout>
+  ),
+  document.getElementById('app')
+);
