02b64b6e775094b9e9219f293f6a5b1070b07324
Jason <jasonventresca@gmail.com>
2018-02-17 20:16:04 -0500

    Added babel plugin: transform-class-properties.

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -5,3 +5,20 @@ import IndecisionApp from './components/IndecisionApp';
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Jason';
+  }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+class NewSyntax {
+  // For defining class properties this way, don't use `var`, `const`, etc.
+  name = 'Alice';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
